b87b4f15fffa5d18bd78d0a1a96ef91854ea3f6a
Jim Balhoff <balhoff@nescent.org>
2011-05-06 13:21:36 -0400

    Updated download URLs.

--- build_hao.sh
+++ build_hao.sh
@@ -2,12 +2,8 @@
 
 export CLASSPATH=.:lib/log4j-1.2.15.jar:lib/oboformat.jar:lib/owlapi-bin.jar
 
-# need to check URL and also generate plain-text, not HTML
-#curl -O http://peet.tamu.edu/projects/32/public/ontology/show_OBO_file
-# this URL is just for testing
-curl -L -O http://purl.obolibrary.org/obo/hao.obo
+curl -L -o hao.obo http://api.hymao.org/api/ontology/obo_file
 java -Xmx2048M org.obolibrary.cli.OBORunner hao.obo -o file://`pwd`/hao_base.owl
-#TODO download image RDF from MX > hao_images.owl
-#curl -L -O http://peet.tamu.edu/projects/32/public/ontology/export_class_depictions -o hao_images.owl
+curl -L -o hao_images.owl http://api.hymao.org/api/ontology/class_depictions
 javac OWLFileMerger.java
 java OWLFileMerger hao_base.owl hao_images.owl hao.owl
