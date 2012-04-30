8f09271d5dc74d40bcf78ee101d82e1d8359dd26
Jim Balhoff <balhoff@nescent.org>
2012-04-30 16:28:24 -0400

    Updated oboformat jar.

--- build_hao.sh
+++ build_hao.sh
@@ -2,8 +2,9 @@
 
 export CLASSPATH=.:lib/log4j-1.2.15.jar:lib/oboformat.jar:lib/owlapi-bin.jar
 
+rm hao.obo hao_base.owl hao_images.owl hao.owl
 curl -L -o hao.obo http://api.hymao.org/api/ontology/obo_file
-java -Xmx2048M org.obolibrary.cli.OBORunner hao.obo -o file://`pwd`/hao_base.owl
+java -Xmx2048M org.obolibrary.cli.OBORunner hao.obo -o hao_base.owl
 curl -L -o hao_images.owl http://api.hymao.org/api/ontology/class_depictions
 javac OWLFileMerger.java
 java OWLFileMerger hao_base.owl hao_images.owl hao.owl
